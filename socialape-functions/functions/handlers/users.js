6cb6e16f101288e56558dc76d31b253e2987e9d5
Alexander Gribenchenko <samnite87@gmail.com>
2019-11-15 10:29:02 +0300

    add "userCredentials" route

--- socialape-functions/functions/handlers/users.js
+++ socialape-functions/functions/handlers/users.js
@@ -100,6 +100,32 @@ exports.addUserDetails = (req, res) => {
     });
 };
 
+// Get own user details
+exports.getAuthenticatedUser = (req, res) => {
+  const userData = {};
+  db.doc(`/users/${req.user.handle}`)
+    .get()
+    .then(doc => {
+      if (doc.exists) {
+        userData.credentials = doc.data();
+        return db
+          .collection('likes')
+          .where('userHandle', '==', req.user.handle)
+          .get();
+      }
+    })
+    .then(data => {
+      userData.likes = [];
+      data.forEach(doc => {
+        userData.likes.push(doc.data());
+      });
+      return res.json(userData);
+    })
+    .catch(err => {
+      return res.status(500).json({ error: err.code });
+    });
+};
+
 // Upload profile user image
 exports.uploadImage = (req, res) => {
   const BusBoy = require('busboy');
--- socialape-functions/functions/index.js
+++ socialape-functions/functions/index.js
@@ -4,16 +4,23 @@ const app = require('express')();
 const FBAuth = require('./util/fbAuth');
 
 const { getAllScreams, postOneScream } = require('./handlers/screams');
-const { signup, login, uploadImage, addUserDetails } = require('./handlers/users');
+const {
+  signup,
+  login,
+  uploadImage,
+  addUserDetails,
+  getAuthenticatedUser,
+} = require('./handlers/users');
 
 // Scream routes
 app.get('/screams', getAllScreams);
 app.post('/scream', FBAuth, postOneScream);
+app.post('/user/image', FBAuth, uploadImage);
+app.post('/user', FBAuth, addUserDetails);
+app.get('/user', FBAuth, getAuthenticatedUser);
 
 // Users routes
 app.post('/signup', signup);
 app.post('/login', login);
-app.post('/user/image', FBAuth, uploadImage);
-app.post('/user', FBAuth, addUserDetails);
 
 exports.api = functions.region('europe-west1').https.onRequest(app);
