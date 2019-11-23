179ca5737bbd8ad7468fb30eac1dc80a23eafa08
Alexander Gribenchenko <samnite87@gmail.com>
2019-11-23 22:57:42 +0300

    add "Profile" component

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import dayjs from 'dayjs';
 import withStyles from '@material-ui/core/styles/withStyles';
 // MUI Stuff
+import LocationOn from '@material-ui/icons/LocationOn';
+import LinkIcon from '@material-ui/icons/Link';
+import CalendarToday from '@material-ui/icons/CalendarToday';
+
+import MuiLink from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import theme from '../util/theme';
 
-const styles = {
+const styles = theme => ({
   paper: {
     padding: 20,
   },
@@ -52,14 +61,71 @@ const styles = {
       margin: '20px 10px',
     },
   },
-};
+});
 
 const Profile = ({
   classes,
-  user: { handle, createdAt, imageUrl, bio, website, location },
-  loading,
+  user: {
+    credentials: { handle, createdAt, imageUrl, bio, website, location },
+    loading,
+    authenticated,
+  },
 }) => {
-  return <div></div>;
+  // eslint-disable-next-line no-nested-ternary
+  return !loading ? (
+    authenticated ? (
+      <Paper className={classes.paper}>
+        <div className={classes.profile}>
+          <div className="image-wrapper">
+            <img src={imageUrl} alt="profile" className="profile-image" />
+          </div>
+          <hr />
+          <div className="profile-details">
+            <MuiLink component={Link} to={`/users/${handle}`} color="primary" variant="h5">
+              @{handle}
+            </MuiLink>
+            <hr />
+            {bio && <Typography variant="body2">{bio}</Typography>}
+            <hr />
+            {location && (
+              <>
+                <LocationOn color="primary" /> <span>{location}</span>
+                <hr />
+              </>
+            )}
+            {website && (
+              <>
+                <LinkIcon color="primary" />
+                <a href={website} target="_blank" rel="noopener noreferrer">
+                  {' '}
+                  {website}
+                </a>
+                <hr />
+              </>
+            )}
+            <CalendarToday color="primary" />{' '}
+            <span>Joined {dayjs(createdAt).format('MMM YYYY')}</span>
+          </div>
+        </div>
+      </Paper>
+    ) : (
+      <Paper className={classes.paper}>
+        <Typography variant="body2" align="center">
+          No profile found, please login again
+        </Typography>
+        <div className={classes.buttons}>
+          <Button variant="contained" color="primary" component={Link} to="/login">
+            Login
+          </Button>
+          <Button variant="contained" color="secondary" component={Link} to="/signup">
+            Signup
+          </Button>
+        </div>
+      </Paper>
+    )
+  ) : (
+    <p>loading...</p>
+  );
 };
 
 const mapStateToProps = state => ({
